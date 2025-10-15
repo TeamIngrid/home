87640f090bc42aecdeea9907111693036445c7aa
ingrid <diaz3@uwindsor.ca>
2025-10-15 14:18:58 -0400

    initial commit
    body of the initial commit

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/TeamIngrid/aboutme.github.io.git";
+const repoURL = "https://github.com/TeamIngrid/TeamIngrid.github.io.git";
 
 ghpages.publish(
   pathname,
