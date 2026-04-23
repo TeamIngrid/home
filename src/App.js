b47fded76fb6354323f42d3b57cd93f4be5b9479
TeamIngrid <diaz3@uwindsor.ca>
2026-04-23 18:37:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -78,14 +78,15 @@ const Home = React.forwardRef((props, ref) => {
 const App = () => {
   const titleRef = React.useRef();
 
+  const basename =
+    process.env.NODE_ENV === "production" ? "/home" : "/";
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+    <BrowserRouter basename={basename}>
       {navBar.show && <Navbar ref={titleRef} />}
       <Routes>
-        <Route path="/" exact element={<Home ref={titleRef} />} />
+        <Route path="/" element={<Home ref={titleRef} />} />
       </Routes>
-      {/* {false && <Route path="/blog" exact component={Blog} />}
-      {false && <Route path="/blog/:id" component={BlogPost} />} */}
       <Footer>
         {getInTouch.show && (
           <GetInTouch
