ac6df86b2d6a876efc558e9be72da4f40ca8d596
Ingrid's Leudatum <diaz3@uwindsor.ca>
2026-04-24 16:21:43 -0400

    Update GetInTouch.jsx

--- src/components/home/GetInTouch.jsx
+++ src/components/home/GetInTouch.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const GetInTouch = ({ heading, message, email }) => {
   return (
     <>
-      <h2 className="display-4 pb-3 text-center">{heading}</h2>
-      <p className="lead text-center pb-3">
-        {message}, <a className="text-decoration-none" href={`mailto:${email}`}>{}</a>.
-      </p>
+      <h1>{heading}</h1>
+      <p>{message}</p>
+      {email && <a href={email}>{email}</a>}
     </>
   );
 };
