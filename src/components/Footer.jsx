8c0b67e614e55efca97b5ccd5e54069420b3dabb
Ingrid's Leudatum <diaz3@uwindsor.ca>
2026-04-27 12:18:04 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,19 @@ import Container from "react-bootstrap/Container";
 const Footer = (props) => {
   const bgStyle = { backgroundColor: "#f5f5f5" };
 
-   return (
-  <footer style={bgStyle} className="mt-auto py-5 text-center ">
-    <Container>
-      {props.children}
-      <p>
-        <small className="text-muted">
-          Have a nice day!
-        </small>
-      </p>
-    </Container>
-  </footer>
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center">
+      <Container>
+        {props.children}
+
+        <p>
+          <small className="text-muted">
+            © {new Date().getFullYear()} Ingrid Diaz
+          </small>
+        </p>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
