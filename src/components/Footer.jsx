1e4427941e310c1b917be5175073d61c93efbd28
Ingrid's Leudatum <diaz3@uwindsor.ca>
2026-04-27 12:16:12 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ import Container from "react-bootstrap/Container";
 const Footer = (props) => {
   const bgStyle = { backgroundColor: "#f5f5f5" };
 
-  return (
    return (
   <footer style={bgStyle} className="mt-auto py-5 text-center ">
     <Container>
